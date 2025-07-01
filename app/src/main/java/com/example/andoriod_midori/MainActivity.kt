4fe5aea428dcf9eb585556c5e0a7a8762a7258d8
nck90 <s2460@e-mirim.hs.kr>
2025-07-01 19:52:08 +0900

    Initial commit: Android Compose project setup

---
+++ app/src/main/java/com/example/andoriod_midori/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.andoriod_midori
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.andoriod_midori.ui.theme.Andoriod_midoriTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Andoriod_midoriTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Andoriod_midoriTheme {
+        Greeting("Android")
+    }
+} 
